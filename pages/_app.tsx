5e9b07fbf781f108cd0c1f50835afac5489f14b6
Madison <madisonkanna@gmail.com>
2021-12-24 23:29:52 -0800

    feat: add chakraprovider and theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,39 @@
-import '../styles/globals.css'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import 'reset-css';
+
+const theme = extendTheme({
+  colors: {
+    gray: {
+      100: '#F5F5F%',
+      200: '#EEEEE',
+      300: '#E0E0E0',
+      400: '#BDBDBD',
+      500: '#9E9E9E',
+      600: '#757575',
+      800: '#424242',
+      900: '#212121',
+    },
+  },
+  components: {
+    Button: {
+      variants: {
+        link: {
+          ':focus': {
+            outline: 'none',
+            boxShadow: 'none',
+          },
+        },
+      },
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />;
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
