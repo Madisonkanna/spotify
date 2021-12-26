cc8fc78202abdd5a2b7dc829b620f169b356ce71
Madison <madisonkanna@gmail.com>
2021-12-25 22:15:54 -0800

    feat(playerlayout): add sidebar, page and player

--- components/PlayerLayout.tsx
+++ components/PlayerLayout.tsx
@@ -3,7 +3,15 @@ import { Box } from "@chakra-ui/layout";
 const PlayerLayout = ({ children }) => {
   return (
     <Box width="100vw" height="100vw">
-      <Box>sidebar</Box>
+      <Box top="0" left="0" width="250px" position="absolute">
+        sidebar
+      </Box>
+      <Box marginBottom="100px" marginLeft="250px">
+        {children}
+      </Box>
+      <Box left="0" bottom="0" position="absolute">
+        player
+      </Box>
     </Box>
   );
 };
